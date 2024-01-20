562db40afcf8d4c9246559cefe967116ad4d449f
unknown <lotaymk@gmail.com>
2024-01-20 17:01:49 -0500

    fix grid in about section

--- src/components/About.js
+++ src/components/About.js
@@ -17,27 +17,19 @@ function About() {
     return(
         <section id="about">
             <h1>{'<'}About Me /{'>'}</h1>
-            {/* <div className="about-text-container">
-                
-                <h3 className="education">I am currently a 3rd year <b>Software Engineering</b> student at Concordia University, Montreal, Canada.</h3>
-            </div> */}
-            <div className="skills-container">
-                <h3>Languages & Tools</h3>
-                <div className="languages-grid-container">
-                    <img className="language-logo" src={javaLogo} alt="logo of java"/> 
-                    <img className="language-logo" src={CLogo} alt="logo of C"/>
-                    <img className="language-logo" src={CPPLogo} alt="logo of C++"/>
-                    <img className="language-logo" src={pythonLogo} alt="logo of Python"/>
-                    <img className="language-logo" src={HTMLLogo} alt="logo of HTML"/>
-                    <img className="language-logo" src={CSSLogo} alt="logo of CSS"/>
-                    <img className="language-logo" src={bootstrapLogo} alt="logo of Bootstrap"/>
-                    <img className="language-logo" src={nodejsLogo} alt="logo of Node.js"/>
-                    <img className="language-logo" src={reactjsLogo} alt="logo of React.js"/>
-                    <img className="language-logo" src={mongoDBLogo} alt="logo of mongoDB"/>
-                    <img className="language-logo" src={gitLogo} alt="logo of Git"/>
-                    <img className="language-logo" src={vscodeLogo} alt="logo of Visual Studio Code"/>
-                    <img className="language-logo" src={powerPlatformLogo} alt="logo of Microsoft Power Platform"/> 
-                </div>
+            <h3 className="education">I am currently a 3rd year <b>Software Engineering</b> student at Concordia University, Montreal, Canada. Here are some technologies I have been working with:</h3>
+            <div className="languages-grid-container">
+                <img className="language-logo" src={javaLogo} alt="logo of java"/> 
+                <img className="language-logo" src={CLogo} alt="logo of C"/>
+                <img className="language-logo" src={CPPLogo} alt="logo of C++"/>
+                <img className="language-logo" src={pythonLogo} alt="logo of Python"/>
+                <img className="language-logo" src={HTMLLogo} alt="logo of HTML"/>
+                <img className="language-logo" src={CSSLogo} alt="logo of CSS"/>
+                <img className="language-logo" src={bootstrapLogo} alt="logo of Bootstrap"/>
+                <img className="language-logo" src={nodejsLogo} alt="logo of Node.js"/>
+                <img className="language-logo" src={reactjsLogo} alt="logo of React.js"/>
+                <img className="language-logo" src={mongoDBLogo} alt="logo of mongoDB"/>                    <img className="language-logo" src={vscodeLogo} alt="logo of Visual Studio Code"/>
+                <img className="language-logo" src={powerPlatformLogo} alt="logo of Microsoft Power Platform"/> 
             </div>
         </section>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ function Home() {
     return(
         <section id="home">
             <div className="home-text-container">
-                {/* <h1>Hello, Manreet here.</h1> */}
                 <h1>Hello, <span class="highlight-name">Manreet</span> here.</h1>
                 <h3>A Full-Stack Developer with a passion for crafting apps that are both user-friendly and accessible.</h3>
             </div>
