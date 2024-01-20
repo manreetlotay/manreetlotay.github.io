a7b09d3eae50c5fac66bcf29717f53e787688b8c
unknown <lotaymk@gmail.com>
2024-01-20 18:59:04 -0500

    Add Material UI project Card

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
                 <h3>A Full-Stack Developer with a passion for crafting apps that are both user-friendly and accessible.</h3>
             </div>
             <div className="home-buttons-container">
-                <button className="resume-btn">Resume</button>
+                <button className="resume-btn">Resume <i class="fa-solid fa-download"></i></button>
                 <button className="contact-btn">Contact Me</button>
             </div>
             <div className="cat-walking"></div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,20 @@
+import img from '../images/reactjs.png';
+
 function Projects() {
     return(
         <section id="projects">
             <h1>{'<'}My Projects {'/>'}</h1>
             <div className="realEstate-project-container projectCard">
                 <h2>Project Name</h2>
-                <h4>Languages</h4>
-                <img src=""></img>
+                <h5>Languages</h5>
+                <img src={img} alt="project image"></img>
                 <h3>Brief Description</h3>
-                <button className="View-GitHub-Btn">View on GitHub</button>
-                <button className="View-Demo-Btn">Watch Demo</button>
-                <ul className="language-container">
-                    <li>ReactJS</li>
-                    <li>Express.js</li>
-                    <li>MongoDB</li>
-                    <li>Node.js</li>
-                </ul>
+                <a href="/" title="View source code on gitHub" target="_blank" rel="noopener">
+                    <i className="fa-brands fa-github"></i>
+                </a>
+                <a href="/" title="View demo on youTube" target="_blank" rel="noopener">
+                    <i className="fa-brands fa-youtube"></i>
+                </a>
                
             </div>
         </section>
