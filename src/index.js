a16887ced0281bc2406700321ac7547747ad8248
unknown <lotaymk@gmail.com>
2024-01-22 01:39:20 -0500

    Separate styling into different files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
+import './styles/navbar.css';
+import './styles/home.css';
+import './styles/about.css';
+import './styles/work.css';
+import './styles/projects.css';
+import './styles/footer.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
