e6d0f3005f3c80a8ff539bccc4f1dce69fc4549b
unknown <lotaymk@gmail.com>
2024-01-22 14:45:15 -0500

    test namechange

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
                 <h3>A Full-Stack Developer with a passion for crafting apps that are both user-friendly and accessible.</h3>
             </div>
             <div className="home-buttons-container">
-                <button className="resume-btn"><a href={CV} title="" download>Resume</a><i class="fa-solid fa-download"></i></button>
+                <button className="resume-btn"><a href={CV} title="" download>Resume </a><i class="fa-solid fa-download"></i></button>
                 <button className="contact-btn"><a href="mailto:lotaymk@gmail.com" title="Send me an email" target="_blank" rel="noopener">Contact Me</a></button>
             </div>
             <div className="cat-walking"></div>
